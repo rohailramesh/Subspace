437af872b236797b57078a5400c3883edf3b71e8
Rohail Ramesh <r.ramesh@se21.qmul.ac.uk>
2023-08-14 17:35:12 +0100

    Feat: Adding form components

--- subspace-app/components/AddScreen.js
+++ subspace-app/components/AddScreen.js
@@ -1,26 +1,25 @@
-// import { View, Text } from "react-native";
-
-// export default function AddScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Add Screen</Text>
-//     </View>
-//   );
-// }
-
-import { useState } from "react";
+import React, { useState } from "react";
 import { supabase } from "../lib/supabase";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+
+const data = [
+  { label: "Paid", value: "Paid" },
+  { label: "Trial", value: "Trial" },
+];
 
 export default function AddScreen({ session }) {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+  const [type, setType] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
 
   const addSubscription = async () => {
     const { data, error } = await supabase.from("subspace_app").insert({
       user_id: session.user.id,
       name: name,
       price: price,
+      type: type,
     });
     if (error) {
       console.log(error);
@@ -45,21 +44,41 @@ export default function AddScreen({ session }) {
         placeholder="Add Subscription Price"
         keyboardType="numeric"
       />
+
+      <Dropdown
+        style={[dropdownStyles.dropdown, isFocus && { borderColor: "blue" }]}
+        data={data}
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={"Subscription Type"}
+        value={type}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setType(item.value);
+          setIsFocus(false);
+        }}
+      />
+
       <Button title="Add Subscription" onPress={addSubscription} />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     padding: 12,
   },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: "stretch",
-  },
-  mt20: {
-    marginTop: 20,
+});
+
+const dropdownStyles = StyleSheet.create({
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
   },
 });
