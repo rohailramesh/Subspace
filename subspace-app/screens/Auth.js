c39355fd6beb66b030d508ed6244e6659038a13f
Rohail Ramesh <rohailramesh@hotmail.com>
2023-08-23 14:14:02 +0100

    Fix: Fixed authentication page

--- subspace-app/screens/Auth.js
+++ subspace-app/screens/Auth.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Alert, StyleSheet, View, KeyboardAvoidingView } from "react-native";
 import { supabase } from "../lib/supabase";
-import { Button, Input, Text } from "react-native-elements";
+import { Button, Input, Icon } from "react-native-elements";
 import { ImageBackground } from "react-native";
 
 export default function Auth() {
@@ -9,6 +9,7 @@ export default function Auth() {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [isSignUp, setIsSignUp] = useState(false);
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
 
   async function handleAuthentication() {
     setLoading(true);
@@ -40,57 +41,72 @@ export default function Auth() {
         style={styles.backgroundImage}
       >
         <View style={styles.container}>
-          <View style={styles.tabs}>
-            <Text
-              style={[
-                styles.tabText,
-                isSignUp ? styles.inactiveTabText : styles.activeTabText,
-              ]}
-              onPress={() => setIsSignUp(false)}
-            >
-              Sign In
-            </Text>
-            <Text
-              style={[
-                styles.tabText,
-                isSignUp ? styles.activeTabText : styles.inactiveTabText,
-              ]}
-              onPress={() => setIsSignUp(true)}
-            >
-              Sign Up
-            </Text>
-          </View>
-
-          <View style={[styles.verticallySpaced, styles.mt20]}>
+          <View style={[styles.verticallySpaced]}>
             <Input
               label="Email"
-              leftIcon={{ type: "font-awesome", name: "envelope" }}
+              leftIcon={{
+                type: "font-awesome",
+                name: "envelope",
+                color: "white",
+              }}
               onChangeText={(text) => setEmail(text)}
               value={email}
               placeholder="email@address.com"
               autoCapitalize="none"
+              style={{ color: "white" }}
             />
           </View>
 
           <View style={styles.verticallySpaced}>
             <Input
               label="Password"
-              leftIcon={{ type: "font-awesome", name: "lock" }}
+              leftIcon={{ type: "font-awesome", name: "lock", color: "white" }}
+              rightIcon={
+                <Icon
+                  type="font-awesome"
+                  name={secureTextEntry ? "eye-slash" : "eye"}
+                  color="white"
+                  onPress={() => setSecureTextEntry(!secureTextEntry)}
+                />
+              }
               onChangeText={(text) => setPassword(text)}
               value={password}
-              secureTextEntry={true}
+              secureTextEntry={secureTextEntry}
               placeholder="Password"
               autoCapitalize="none"
+              style={{ color: "white" }}
             />
           </View>
 
-          <View style={[styles.verticallySpaced, styles.mt50]}>
+          <View style={[styles.verticallySpaced]}>
             <Button
               title={isSignUp ? "Sign Up" : "Sign In"}
               disabled={loading}
               onPress={() => handleAuthentication()}
               buttonStyle={styles.button} // Customize button background color
               titleStyle={styles.buttonText} // Customize button text color
+            />
+          </View>
+          <View style={styles.tabs}>
+            <Button
+              title="Sign In"
+              type="clear"
+              titleStyle={[
+                styles.tabText,
+                isSignUp ? styles.inactiveTabText : styles.activeTabText,
+              ]}
+              onPress={() => setIsSignUp(false)}
+              buttonStyle={[styles.button, styles.roundedButton]}
+            />
+            <Button
+              title="Sign Up"
+              type="clear"
+              titleStyle={[
+                styles.tabText,
+                isSignUp ? styles.activeTabText : styles.inactiveTabText,
+              ]}
+              onPress={() => setIsSignUp(true)}
+              buttonStyle={[styles.button, styles.roundedButton]}
             />
           </View>
         </View>
@@ -104,6 +120,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center", // Center vertically
     padding: 12,
+    marginTop: 20,
   },
   backgroundImage: {
     flex: 1, // Take up the entire screen
@@ -113,11 +130,6 @@ const styles = StyleSheet.create({
     paddingTop: 4,
     paddingBottom: 4,
     alignSelf: "stretch",
-  },
-  mt20: {
-    marginTop: 150,
-  },
-  mt50: {
     marginTop: 20,
   },
   button: {
@@ -128,139 +140,26 @@ const styles = StyleSheet.create({
   },
   tabs: {
     flexDirection: "row",
-    justifyContent: "space-around",
-    marginBottom: 20,
+    justifyContent: "space-between",
+    marginBottom: 100,
+    paddingRight: 20,
+    gap: 5,
   },
   tabText: {
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: "bold",
     textAlign: "center",
-    padding: 10,
+    padding: 5,
     width: "50%",
+  },
+  inactiveTabText: {
+    color: "grey",
   },
   activeTabText: {
     color: "black",
   },
-  inactiveTabText: {
-    color: "gray",
+
+  roundedButton: {
+    borderRadius: 5, // Adjust the value as needed
   },
 });
-
-// import React, { useState } from "react";
-// import { Alert, StyleSheet, View, KeyboardAvoidingView } from "react-native";
-// import { supabase } from "../lib/supabase";
-// import { Button, Input } from "react-native-elements";
-// import { ImageBackground } from "react-native";
-
-// export default function Auth() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [loading, setLoading] = useState(false);
-
-//   async function signInWithEmail() {
-//     setLoading(true);
-
-//     const { error } = await supabase.auth.signInWithPassword({
-//       email: email,
-//       password: password,
-//     });
-
-//     if (error) Alert.alert(error.message);
-
-//     setLoading(false);
-//   }
-
-//   async function signUpWithEmail() {
-//     setLoading(true);
-
-//     const { error } = await supabase.auth.signUp({
-//       email: email,
-//       password: password,
-//     });
-
-//     if (error) Alert.alert(error.message);
-
-//     setLoading(false);
-//   }
-
-//   return (
-//     <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-//       <ImageBackground
-//         source={require("../assets/login-bg.jpg")}
-//         style={styles.backgroundImage}
-//       >
-//         <View style={[styles.verticallySpaced, styles.mt20]}>
-//           <Input
-//             label="Email"
-//             leftIcon={{ type: "font-awesome", name: "envelope" }}
-//             onChangeText={(text) => setEmail(text)}
-//             value={email}
-//             placeholder="email@address.com"
-//             autoCapitalize={"none"}
-//           />
-//         </View>
-
-//         <View style={styles.verticallySpaced}>
-//           <Input
-//             label="Password"
-//             leftIcon={{ type: "font-awesome", name: "lock" }}
-//             onChangeText={(text) => setPassword(text)}
-//             value={password}
-//             secureTextEntry={true}
-//             placeholder="Password"
-//             autoCapitalize={"none"}
-//           />
-//         </View>
-
-//         <View style={[styles.verticallySpaced, styles.mt50]}>
-//           <Button
-//             title="Sign in"
-//             disabled={loading}
-//             onPress={() => signInWithEmail()}
-//             buttonStyle={styles.button} // Customize button background color
-//             titleStyle={styles.buttonText} // Customize button text color
-//           />
-//         </View>
-
-//         <View style={styles.verticallySpaced}>
-//           <Button
-//             title="Sign up"
-//             disabled={loading}
-//             onPress={() => signUpWithEmail()}
-//             buttonStyle={styles.button} // Customize button background color
-//             titleStyle={styles.buttonText} // Customize button text color
-//           />
-//         </View>
-//       </ImageBackground>
-//     </KeyboardAvoidingView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center", // Center vertically
-//     padding: 12,
-//   },
-//   backgroundImage: {
-//     flex: 1, // Take up the entire screen
-//     resizeMode: "cover", // Adjust the image size to cover the entire container
-//   },
-//   verticallySpaced: {
-//     paddingTop: 4,
-//     paddingBottom: 4,
-//     alignSelf: "stretch",
-//   },
-//   mt20: {
-//     marginTop: 150,
-//   },
-//   mt50: {
-//     marginTop: 20,
-//   },
-//   button: {
-//     backgroundColor: "white", // Customize button background color
-//   },
-//   buttonText: {
-//     color: "black", // Customize button text color
-//   },
-// });
