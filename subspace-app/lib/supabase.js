4c0ab6a5d0a5bed2a35a03ddd16685bf53f401c4
Rohail Ramesh <rohailramesh@hotmail.com>
2023-09-02 15:45:55 +0100

    Fix: Removed comments from supabase.js

--- subspace-app/lib/supabase.js
+++ subspace-app/lib/supabase.js
@@ -1,31 +1,3 @@
-// import "react-native-url-polyfill/auto";
-// import * as SecureStore from "expo-secure-store";
-// import { createClient } from "@supabase/supabase-js";
-
-// const ExpoSecureStoreAdapter = {
-//   getItem: (key) => {
-//     return SecureStore.getItemAsync(key);
-//   },
-//   setItem: (key, value) => {
-//     SecureStore.setItemAsync(key, value);
-//   },
-//   removeItem: (key) => {
-//     SecureStore.deleteItemAsync(key);
-//   },
-// };
-
-// const supabaseUrl = "https://iuvfokhjduqlumrlutlh.supabase.co";
-// const supabaseAnonKey =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1dmZva2hqZHVxbHVtcmx1dGxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI1NDg0NzcsImV4cCI6MjAwODEyNDQ3N30.1nfFxw3kXFBmkcJrH38E2cQCksR96iBABK9XvBvRYZk";
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-//   auth: {
-//     storage: ExpoSecureStoreAdapter,
-//     autoRefreshToken: true,
-//     persistSession: true,
-//     detectSessionInUrl: false,
-//   },
-// });
 import "react-native-url-polyfill/auto";
 import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
